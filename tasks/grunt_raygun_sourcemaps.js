80d3ac2adfd79cfef6d5c02606344a0fd3d550e2
Marcus Pohorely <office@clubdesign.at>
2015-08-08 01:56:20 +0200

    Fixed Async Error

--- tasks/grunt_raygun_sourcemaps.js
+++ tasks/grunt_raygun_sourcemaps.js
@@ -5,135 +5,135 @@
  * Copyright (c) 2015 Marcus Pohorely
  * Licensed under the MIT license.
  */
- 'use strict';
+'use strict';
 
- var async = require('async');
+var async = require('async');
 
- module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-	var fs = require('fs');	
-	var rest = require('restler');
+  var fs = require('fs');
+  var rest = require('restler');
+  var chalk = require('chalk');
 
-	grunt.registerMultiTask('raygun_sourcemaps', 'Grunt plugin to upload multiple sourcemap files to Raygun.io', function() {
-		
-		var done = this.async();
+  grunt.registerMultiTask('raygun_sourcemaps', 'Grunt plugin to upload multiple sourcemap files to Raygun.io', function() {
+    var done = this.async();
 
-		var options = this.options({
-			raygun_app_id: "",
-			raygun_external_token: "",
-			raygun_url: "",
-			js_url_prefix: "",
-			test_run: false,
-			prepareUrlParam: function( abspath ) {
-				return filename;
-			}
-		});
+    var options = this.options({
+      raygun_app_id: "",
+      raygun_external_token: "",
+      raygun_url: "",
+      js_url_prefix: "",
+      test_run: false,
+      prepareUrlParam: function(abspath) {
+        return abspath;
+      }
+    });
 
-		// Abort if Raygun URL is empty
-		if( options.raygun_app_id === "" ) {
-			console.log("Raygun Url missing");
-			return;
-		} else {
-			options.raygun_url = "https://app.raygun.io/upload/jssymbols/" + options.raygun_app_id + "?authToken=" + options.raygun_external_token;	
-		}
+    if (options.raygun_app_id === "") {
+      console.log("Raygun Url missing");
+      return;
+    } else {
+      options.raygun_url = "https://app.raygun.io/upload/jssymbols/" + options.raygun_app_id + "?authToken=" + options.raygun_external_token;
+    }
 
-		if( options.test_run ) {
-			grunt.log.warn('Test Rund is enabled. Files will not be uploaded.');
-		}
+    if (options.test_run) {
+      grunt.log.warn('Test Rund is enabled. Files will not be uploaded.');
+    }
 
-		async.eachSeries( this.files, function( f, nextFileObj ) {
+    async.eachSeries(this.files, function(f, nextFileObj) {
 
-			var files = f.src.filter( function( filepath ) {
+      var files = f.src.filter(function(filepath) {
 
-				if( !grunt.file.exists( filepath ) ) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
 
-			});
+      });
 
-			if (files.length === 0) {
-				if (f.src.length < 1) {
-					grunt.log.warn('Destination ' + chalk.cyan(destFile) + ' not written because no source files were found.');
-				}
-				return nextFileObj();
-			}
+      if (files.length === 0) {
+        if (f.src.length < 1) {
+          grunt.log.warn('Destination ' + chalk.cyan(f.src) + ' not written because no source files were found.');
+        }
+        return nextFileObj();
+      }
 
-			async.concatSeries(files, function(file, next) {
+      async.concatSeries(files, function(file, next) {
 
-				var file_url = options.js_url_prefix + options.prepareUrlParam( file.replace(".map", "") );
+        var file_url = options.js_url_prefix + options.prepareUrlParam(file.replace(".map", ""));
 
-				if( !options.test_run ) {
-					uploadToRaygun( options.raygun_url, file, file_url, function( has_err, err ) {
+        if (!options.test_run) {
+          uploadToRaygun(options.raygun_url, file, file_url, function(has_err, err) {
 
-						if( !has_err ) {
-							grunt.log.ok('Upload successful of "' + file + '"');
-							process.nextTick(next);
-						} else {
-							grunt.log.warn('Upload failed of "' + file + '"');
-							nextFileObj(err);
-						}
+            if (!has_err) {
+              grunt.log.ok('Upload successful of "' + file + '"');
+              process.nextTick(next);
+            } else {
+              grunt.log.warn('Upload failed of "' + file + '"');
+              nextFileObj(err);
+            }
 
-					});
-				} else {
-					grunt.log.write(">> File: " + file + " >> " + file_url + "\n");
-					process.nextTick(next);
-				}
+          });
+        } else {
+          grunt.log.write(">> File: " + file + " >> " + file_url + "\n");
+          process.nextTick(next);
+        }
 
-			}, function() {});
+      }, function() {
+        nextFileObj();
+      });
 
-			return nextFileObj();
-		});
+    }, done);
 
-	});
+  });
 
-	var uploadToRaygun = function( raygun_url, file, file_url, callback ) {
+  var uploadToRaygun = function(raygun_url, file, file_url, callback) {
 
-		fs.stat( file, function( err, stats ) {
+    fs.stat(file, function(err, stats) {
 
-			if( err ) {
-				callback( true, err );
-				return;
-			}
+      if (err) {
+        callback(true, err);
+        return;
+      }
 
-			if( !stats.isFile() ) {
-				callback( true, "no_file" );
-				return;	
-			}
+      if (!stats.isFile()) {
+        callback(true, "no_file");
+        return;
+      }
 
-			var file_size = stats.size;
-			var request_data = {
-				file: rest.file( file, null, file_size, null, null ),
-				url: file_url
-			};
-			grunt.log.writeln('Uploading "' + file + '"');
+      var file_size = stats.size;
+      var request_data = {
+        file: rest.file(file, null, file_size, null, null),
+        url: file_url
+      };
 
-			rest.post( raygun_url, {
-				rejectUnauthorized: false,
-				followRedirects: true,
-				headers: {},
-				multipart: true,
-				data: request_data
-			}).on('error',function(e){
-				grunt.fail.warn('Failed uploading (error code: ' + e.message + ')'); 
-			}).on('complete', function( jdata, response ) {
-				if (response !== null && response.statusCode >= 200 && response.statusCode < 300) {
+      rest.post(raygun_url, {
+        rejectUnauthorized: false,
+        followRedirects: true,
+        headers: {},
+        multipart: true,
+        data: request_data
+      }).on('error', function(e) {
+        grunt.fail.warn('Failed uploading (error code: ' + e.message + ')');
+        callback(true, e.message);
+      }).on('complete', function(jdata, response) {
+        if (response !== null && response.statusCode >= 200 && response.statusCode < 300) {
 
-					if( jdata.Status === "Success" ) {
-						callback( false );
-					} else {
-						callback( true, jdata.Message );
-					}
-					
-				} else {
-					callback( true, 'Failed uploading "' + file + '" (status code: ' + response.statusCode + ')' );
-				}
-			});
+          if (jdata.Status === "Success") {
+            callback(false);
+          } else {
+            callback(true, jdata.Message);
+          }
 
-		});
+        } else {
+          callback(true, 'Failed uploading "' + file + '" (status code: ' + response.statusCode + ')');
+        }
+      });
 
-	};
+    });
+
+  };
 
 };
